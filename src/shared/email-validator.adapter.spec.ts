f727c5b1a4976f62c949d795af0085f90f5a1f70
Filipe Siqueira <filipedev040990@gmail.com>
2022-11-15 15:26:33 -0300

    feat: ensure EmailValidator return true if valiadtor return true

--- src/shared/email-validator.adapter.spec.ts
+++ src/shared/email-validator.adapter.spec.ts
@@ -1,15 +1,27 @@
 import { EmailValidatorAdapter } from './email-validator.adapter'
 import validator from 'validator'
 
+jest.mock('validator', () => ({
+  async isEmail () {
+    return await Promise.resolve(true)
+  }
+}))
+
 const makeSut = (): EmailValidatorAdapter => {
   return new EmailValidatorAdapter()
 }
 
 describe('', () => {
-  test('should ...', async () => {
+  test('should call validator with correct email', async () => {
     const sut = makeSut()
     const spy = jest.spyOn(validator, 'isEmail')
     await sut.execute('anyEmail@email.com')
     expect(spy).toHaveBeenCalledWith('anyEmail@email.com')
+  })
+
+  test('should return true if validator return true', async () => {
+    const sut = makeSut()
+    const response = await sut.execute('anyEmail@email.com')
+    expect(response).toBeTruthy()
   })
 })
--- src/shared/email-validator.adapter.ts
+++ src/shared/email-validator.adapter.ts
@@ -3,7 +3,6 @@ import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidatorInterface {
   async execute (email: string): Promise<boolean> {
-    await validator.isEmail(email)
-    return null
+    return validator.isEmail(email)
   }
 }
