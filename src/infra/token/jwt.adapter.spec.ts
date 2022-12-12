247c583495c979849bab1f64589afc39b3c768e4
Filipe Siqueira <filipedev040990@gmail.com>
2022-12-12 17:52:35 -0300

    feat: ensure JwtAdapter call verify with correct values and with expiresIn in constructor

--- src/infra/token/jwt.adapter.spec.ts
+++ src/infra/token/jwt.adapter.spec.ts
@@ -5,6 +5,9 @@ import { JwtAdapter } from './jwt.adapter'
 jest.mock('jsonwebtoken', () => ({
   sign: async () => {
     return await Promise.resolve('anyToken')
+  },
+  verify: async () => {
+    return await Promise.resolve('anyTokenDecrypted')
   }
 }))
 
@@ -19,32 +22,44 @@ const makeSut = (): SutType => {
   return { sut }
 }
 
-describe('', () => {
-  test('should call JwtAdapter with correct values and expiresIn in constructor', async () => {
-    const { sut } = makeSut()
-    const spy = jest.spyOn(jwt, 'sign')
-    await sut.encrypt({ account_id: 'anyId' })
-    expect(spy).toHaveBeenCalledTimes(1)
-    expect(spy).toHaveBeenCalledWith({ account_id: 'anyId' }, secretKey, { expiresIn })
+describe('JwtAdapter', () => {
+  describe('encrypt', () => {
+    test('should call JwtAdapter with correct values and expiresIn in constructor', async () => {
+      const { sut } = makeSut()
+      const spy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt({ account_id: 'anyId' })
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith({ account_id: 'anyId' }, secretKey, { expiresIn })
+    })
+
+    test('should call JwtAdapter with correct values and without expiresIn in constructor', async () => {
+      const sut = new JwtAdapter(secretKey)
+      const spy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt({ account_id: 'anyId' })
+      expect(spy).toHaveBeenCalledWith({ account_id: 'anyId' }, secretKey, { expiresIn })
+    })
+
+    test('should throw if JwtAdapter throws', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+      const response = sut.encrypt({ account_id: 'anyId' })
+      await expect(response).rejects.toThrow()
+    })
+
+    test('should return an token on success', async () => {
+      const { sut } = makeSut()
+      const token = await sut.encrypt({ account_id: 'anyId' })
+      expect(token).toBe('anyToken')
+    })
   })
 
-  test('should call JwtAdapter with correct values and without expiresIn in constructor', async () => {
-    const sut = new JwtAdapter(secretKey)
-    const spy = jest.spyOn(jwt, 'sign')
-    await sut.encrypt({ account_id: 'anyId' })
-    expect(spy).toHaveBeenCalledWith({ account_id: 'anyId' }, secretKey, { expiresIn })
-  })
-
-  test('should throw if JwtAdapter throws', async () => {
-    const { sut } = makeSut()
-    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
-    const response = sut.encrypt({ account_id: 'anyId' })
-    await expect(response).rejects.toThrow()
-  })
-
-  test('should return an token on success', async () => {
-    const { sut } = makeSut()
-    const token = await sut.encrypt({ account_id: 'anyId' })
-    expect(token).toBe('anyToken')
+  describe('sign', () => {
+    test('should call JwtAdapter with correct values and expiresIn in constructor', async () => {
+      const { sut } = makeSut()
+      const spy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('anyToken')
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith('anyToken', secretKey)
+    })
   })
 })
--- src/infra/token/jwt.adapter.ts
+++ src/infra/token/jwt.adapter.ts
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken'
+import { DecrypterAdapterInterface } from '../../data/interfaces/decrypter.adapter.interface'
 import { EncrypterAdapterInterface } from '../../data/interfaces/encrypter.adapter.interface'
 import env from '../../main/env'
 
-export class JwtAdapter implements EncrypterAdapterInterface {
+export class JwtAdapter implements EncrypterAdapterInterface, DecrypterAdapterInterface {
   constructor (
     private readonly secretKey: string,
     private readonly expiresIn?: string
@@ -10,5 +11,10 @@ export class JwtAdapter implements EncrypterAdapterInterface {
 
   async encrypt (payload: object): Promise<string> {
     return jwt.sign(payload, this.secretKey, { expiresIn: this.expiresIn || env.encrypter.expiresIn })
+  }
+
+  async decrypt (value: string): Promise<string> {
+    jwt.verify(value, this.secretKey)
+    return null
   }
 }
