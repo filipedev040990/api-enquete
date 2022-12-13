f9606f51e7e08660d6b04e657716bf12f9297865
Filipe Siqueira <filipedev040990@gmail.com>
2022-12-13 19:30:31 -0300

    refactor: rename survey folder to add

--- src/main/factories/survey/add/add-survey.factory.ts
+++ src/main/factories/survey/add/add-survey.factory.ts
@@ -1,7 +1,7 @@
 import { AddSurveyUseCase } from '../../../../data/use-cases/surveys/add-survey.usecase'
 import { LogRepository } from '../../../../infra/database/mongodb/repositories/log/log.repository'
 import { SurveyRepository } from '../../../../infra/database/mongodb/repositories/survey/survey.repository'
-import { AddSurveyController } from '../../../../presentation/controllers/survey/add-survey/add-survey'
+import { AddSurveyController } from '../../../../presentation/controllers/survey/add/add-survey'
 import { ControllerInterface } from '../../../../presentation/interfaces'
 import { LogControllerDecorator } from '../../../decorators/log.decorator'
 import { makeAddSurveyValidationComposite } from './add-survey-validation.factory'
