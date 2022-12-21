a143ae7031591cab4ef803f2a2c53bbf67576340
Filipe Siqueira <filipedev040990@gmail.com>
2022-12-21 18:34:22 -0300

    feat: add swagger api

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import { router } from './routes'
+import setupSwagger from './config/swagger'
 
 const app = express()
-
+setupSwagger(app)
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API desenvolvida no curso do Rodrigo Manguinho',
+    version: '1.0.0'
+  }
+}
