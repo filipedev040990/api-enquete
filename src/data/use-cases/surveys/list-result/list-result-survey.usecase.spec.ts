52f398247bc3cee6587fdf1b39967be88218e5c5
Filipe Siqueira <filipedev040990@gmail.com>
2022-12-28 18:07:55 -0300

    feat: ensure ListResultSurveyUseCase call SurveyRepository.getById if is empty answers

--- src/data/use-cases/surveys/list-result/list-result-survey.usecase.spec.ts
+++ src/data/use-cases/surveys/list-result/list-result-survey.usecase.spec.ts
@@ -1,4 +1,6 @@
+import { GetSurveyByIdRepositoryInterface } from '@/data/interfaces/get-survey-repository.interface'
 import { ListResultSurveyRepositoryInterface } from '@/data/interfaces/list-result-survey-repository.interface'
+import { SurveyModel } from '@/domain/models/survey.model'
 import { ListResultSurveyUseCase } from './list-result-survey.usecase'
 
 const fakeResult = {
@@ -21,38 +23,62 @@ const fakeResult = {
   ]
 }
 
-type SutType = {
-  sut: ListResultSurveyUseCase
-  surveyRepositoryStub: ListResultSurveyRepositoryInterface
-}
-const makeSut = (): SutType => {
-  const surveyRepositoryStub = makeSurveyRepositoryStub()
-  const sut = new ListResultSurveyUseCase(surveyRepositoryStub)
-  return { sut, surveyRepositoryStub }
+const fakeSurvey: SurveyModel = {
+  id: 'anyId',
+  question: 'Any question',
+  answers: [{
+    image: 'any Image',
+    answer: 'Any Answer'
+  }, {
+    image: 'any Image',
+    answer: 'Another Answer'
+  }],
+  date: new Date()
 }
 
-const makeSurveyRepositoryStub = (): ListResultSurveyRepositoryInterface => {
-  class SurveyRepositoryStub implements ListResultSurveyRepositoryInterface {
+type SutType = {
+  sut: ListResultSurveyUseCase
+  surveyAnswerRepositoryStub: ListResultSurveyRepositoryInterface
+  surveyRepositoryStub: GetSurveyByIdRepositoryInterface
+}
+const makeSut = (): SutType => {
+  const surveyAnswerRepositoryStub = makesurveyAnswerRepositoryStub()
+  const surveyRepositoryStub = makesurveyRepositoryStub()
+  const sut = new ListResultSurveyUseCase(surveyAnswerRepositoryStub, surveyRepositoryStub)
+  return { sut, surveyAnswerRepositoryStub, surveyRepositoryStub }
+}
+
+const makesurveyAnswerRepositoryStub = (): ListResultSurveyRepositoryInterface => {
+  class SurveyAnswerRepositoryStub implements ListResultSurveyRepositoryInterface {
     async getBySurveyIdAndAccountId (surveyId: string, accountId: string): Promise<any> {
       return await Promise.resolve(fakeResult)
+    }
+  }
+  return new SurveyAnswerRepositoryStub()
+}
+
+const makesurveyRepositoryStub = (): GetSurveyByIdRepositoryInterface => {
+  class SurveyRepositoryStub implements GetSurveyByIdRepositoryInterface {
+    async getById (id: string): Promise<SurveyModel> {
+      return await Promise.resolve(fakeSurvey)
     }
   }
   return new SurveyRepositoryStub()
 }
 
 describe('ListResultSurveyUseCase', () => {
-  test('should call Survey repository once and with correct values', async () => {
-    const { sut, surveyRepositoryStub } = makeSut()
-    const spy = jest.spyOn(surveyRepositoryStub, 'getBySurveyIdAndAccountId')
+  test('should call SurveyAnswerRepository.getBySurveyIdAndAccountId once and with correct values', async () => {
+    const { sut, surveyAnswerRepositoryStub } = makeSut()
+    const spy = jest.spyOn(surveyAnswerRepositoryStub, 'getBySurveyIdAndAccountId')
 
     await sut.execute('anySurveyId', 'anyAccountId')
     expect(spy).toBeCalledTimes(1)
     expect(spy).toHaveBeenCalledWith('anySurveyId', 'anyAccountId')
   })
 
-  test('should return server error if Survey repository throw an exception', async () => {
-    const { sut, surveyRepositoryStub } = makeSut()
-    jest.spyOn(surveyRepositoryStub, 'getBySurveyIdAndAccountId').mockImplementationOnce(() => {
+  test('should return server error if SurveyAnswerRepository.getBySurveyIdAndAccountId throw an exception', async () => {
+    const { sut, surveyAnswerRepositoryStub } = makeSut()
+    jest.spyOn(surveyAnswerRepositoryStub, 'getBySurveyIdAndAccountId').mockImplementationOnce(() => {
       throw new Error()
     })
 
@@ -70,5 +96,16 @@ describe('ListResultSurveyUseCase', () => {
     expect(response).toHaveProperty('surveyId')
     expect(response).toHaveProperty('question')
     expect(response).toHaveProperty('answers')
+  })
+
+  test('should call SurveyRepository.getById if is empty answers', async () => {
+    const { sut, surveyRepositoryStub, surveyAnswerRepositoryStub } = makeSut()
+    const spy = jest.spyOn(surveyRepositoryStub, 'getById')
+
+    jest.spyOn(surveyAnswerRepositoryStub, 'getBySurveyIdAndAccountId').mockReturnValueOnce(Promise.resolve(null))
+
+    await sut.execute('anySurveyId', 'anyAccountId')
+    expect(spy).toBeCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith('anySurveyId')
   })
 })
--- src/data/use-cases/surveys/list-result/list-result-survey.usecase.ts
+++ src/data/use-cases/surveys/list-result/list-result-survey.usecase.ts
@@ -1,10 +1,35 @@
+import { GetSurveyByIdRepositoryInterface } from '@/data/interfaces/get-survey-repository.interface'
 import { ListResultSurveyRepositoryInterface } from '@/data/interfaces/list-result-survey-repository.interface'
 import { SurveyResultModel } from '@/domain/models/survey-result.model'
+import { SurveyModel } from '@/domain/models/survey.model'
 import { ListResultSurveyUseCaseInterface } from '@/domain/use-cases/survey/list-result-survey.interface'
 
 export class ListResultSurveyUseCase implements ListResultSurveyUseCaseInterface {
-  constructor (private readonly surveyRepository: ListResultSurveyRepositoryInterface) {}
+  constructor (
+    private readonly listResultSurveyRepository: ListResultSurveyRepositoryInterface,
+    private readonly getSurveyByIdRepository: GetSurveyByIdRepositoryInterface
+  ) {}
+
   async execute (surveyId: string, accountId: string): Promise<SurveyResultModel> {
-    return await this.surveyRepository.getBySurveyIdAndAccountId(surveyId, accountId)
+    let surveyResult = await this.listResultSurveyRepository.getBySurveyIdAndAccountId(surveyId, accountId)
+    if (!surveyResult) {
+      surveyResult = await this.getSurveyByIdRepository.getById(surveyId)
+      surveyResult = this.makeEmptyResult(surveyResult)
+    }
+    return surveyResult
+  }
+
+  private makeEmptyResult (survey: SurveyModel): SurveyResultModel {
+    return {
+      surveyId: survey.id,
+      question: survey.question,
+      date: survey.date,
+      answers: survey.answers.map(answer => ({
+        ...answer,
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }))
+    }
   }
 }
